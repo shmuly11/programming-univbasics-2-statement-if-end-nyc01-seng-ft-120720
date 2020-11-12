ee3fc9c636c94de11cfce203da119c13bacf5199
shmuly levitin <shmuly11@gmail.com>
2020-11-12 22:33:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,7 +3,7 @@ chance_of_rain = 0.2
 
 if chance_of_rain <= 0.25
   puts "pack sun shelter"
-elseif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "umbrealla"
 else
   puts "Stay home"
